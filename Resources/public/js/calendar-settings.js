8f6dfaf2808a48aed8c4fb99731d3bb176b7360b
dante2008 <space_energy001@yahoo.com>
2014-04-21 18:10:54 +0800

    Fix JavaScript file indentation inconsistency.

--- Resources/public/js/calendar-settings.js
+++ Resources/public/js/calendar-settings.js
@@ -1,35 +1,34 @@
 $(function () {
-		var date = new Date();
-		var d = date.getDate();
-		var m = date.getMonth();
-		var y = date.getFullYear();
-		
-		$('#calendar-holder').fullCalendar({
-			header: {
-				left: 'prev, next',
-				center: 'title',
-				right: 'month,basicWeek,basicDay,'
-			},
-			lazyFetching:true,
-            timeFormat: {
-                    // for agendaWeek and agendaDay
-                    agenda: 'h:mmt', // 5:00 - 6:30
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
 
-                    // for all other views
-                    '': 'h:mmt'            // 7p
-            },
-			eventSources: [
-                    {
-                        url: Routing.generate('fullcalendar_loader'), 
-						type: 'POST',
-						// A way to add custom filters to your event listeners
-						data: {
+    $('#calendar-holder').fullCalendar({
+        header: {
+            left: 'prev, next',
+            center: 'title',
+            right: 'month, basicWeek, basicDay,'
+        },
+        lazyFetching: true,
+        timeFormat: {
+            // for agendaWeek and agendaDay
+            agenda: 'h:mmt',    // 5:00 - 6:30
 
-                        },
-                        error: function() {
-                           //alert('There was an error while fetching Google Calendar!');
-                        }
-                    }
-			]
-		});
+            // for all other views
+            '': 'h:mmt'         // 7p
+        },
+        eventSources: [
+            {
+                url: Routing.generate('fullcalendar_loader'),
+                type: 'POST',
+                // A way to add custom filters to your event listeners
+                data: {
+                },
+                error: function() {
+                   //alert('There was an error while fetching Google Calendar!');
+                }
+            }
+        ]
+    });
 });
