1bd71821f2ca727671967ab8b5e05425145b7c11
marcaube <marc@tourismecie.com>
2014-04-24 11:44:59 -0400

    Add support for non-standard fields, fixes #19

--- Tests/Entity/EventEntityTest.php
+++ Tests/Entity/EventEntityTest.php
@@ -2,6 +2,8 @@
 
 namespace ADesigns\CalendarBundle\Tests\Entity;
 
+use ADesigns\CalendarBundle\Entity\EventEntity;
+
 class EventEntityTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstructBasic()
@@ -50,5 +52,21 @@ class EventEntityTest extends \PHPUnit_Framework_TestCase
         );
         
         $this->assertEquals($entityArray, $arrayCheck);
+    }
+
+    public function testNonStandardFields()
+    {
+        $event = new EventEntity('Test', new \DateTime('2012-01-01 00:00:00'), new \DateTime('2012-01-01 01:00:00'));
+        $event->addField('description', 'Event descriptions');
+
+        $expectedArray = array(
+            'title' => 'Test',
+            'start' => date("Y-m-d\TH:i:sP", strtotime('2012-01-01 00:00:00')),
+            'end' => date("Y-m-d\TH:i:sP", strtotime('2012-01-01 01:00:00')),
+            'allDay' => false,
+            'description' => 'Event descriptions'
+        );
+
+        $this->assertEquals($event->toArray(), $expectedArray);
     }
 }
